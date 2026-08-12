b70f5bc430bdfe593e81487928eb8eaa3f3022c9
shubhamq2 <dudoreward@gmail.com>
2026-08-12 16:37:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -430,7 +430,7 @@ app.get('/api/user/history/:userId', verifyAppToken, async (req, res) => {
 });
 
 // ============================================================================
-// 10. APP DYNAMIC CONFIGS (Banners, Social, Tasks, Video)
+// 10. APP DYNAMIC CONFIGS (Banners, Social, Tasks, Video, Toggles)
 // ============================================================================
 app.get('/api/app-config/:type', async (req, res) => {
     const type = req.params.type;
@@ -442,6 +442,7 @@ app.get('/api/app-config/:type', async (req, res) => {
         else if (type === 'smart-tasks') dbPath = 'global_system_runtime_configurations/smart_tasks';
         else if (type === 'offerwalls') dbPath = 'global_system_runtime_configurations/offerwall_configurations';
         else if (type === 'secure-cloud') dbPath = 'global_system_runtime_configurations/watch_ads_configuration';
+        else if (type === 'module-toggles') dbPath = 'global_system_runtime_configurations/module_toggles'; // 🔥 YAHAN HIDE/SHOW ADD KIYA HAI
         else return res.status(400).json({ success: false, message: "Invalid Config Type" });
 
         const snap = await db.ref(dbPath).once('value');
